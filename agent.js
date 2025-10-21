858c6eff3a509ddab681b93b0b514721a5cde842
JonaCodes <jonathanfarache@gmail.com>
2025-10-21 14:35:45 +0300

    project scaffold

--- agent.js
+++ agent.js
@@ -1,25 +1,8 @@
 import { askGeminiWithMessages } from './llmService.js';
 import { getUserInput } from './cli-io.js';
 
-const memory = [{
-    role: 'model',
-    parts: [{ text: 'Always respond in valid JSON, with this format: {"answer": string}' }],
-}]
-
-const addToMemory = (role, text) => {
-    memory.push({ role, parts: [{ text }] })
-}
-
 async function main() {
-    while (true) {
-        const userInput = await getUserInput('You: ');
-        addToMemory('user', userInput);
 
-        const llmResponse = await askGeminiWithMessages(memory);
-        addToMemory('model', llmResponse.answer);
-
-        console.log('Gemini:', llmResponse.answer);
-    }
 }
 
 main();
